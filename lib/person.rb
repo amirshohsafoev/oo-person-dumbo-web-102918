9e78da1222839a6d4b803a86313dc1335462bdda
amirshohsafoev <amirsafoev0@gmail.com>
2018-10-31 13:25:26 -0400

    created person class

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
 # your code goes here
+class Person
+  attr_reader :name
+  attr_accessor :bank_account
+  def happiness_points=(arg)
+    @happiness_points = arg
+  end
+  def happiness_points
+    @happiness_points
+  end
+  def hygiene_points=(arg)
+    @hygiene_points = arg
+  end
+  def hygiene_points
+    @hygiene_points
+  end
+
+
+end
